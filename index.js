3162d44d90849071f9c1367fe5ef8ddbb9bf325b
kaedinscott12 <kae_s@knights.ucf.edu>
2023-02-02 13:42:26 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,9 @@
+let quotes = [
+  "To the well-organized mind, death is but the next great adventure.",
+  "It matters not what someone is born, but what they grow to be.",
+  "Life's simple: You make choices and you don't look back.",
+];
+
+let quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+console.log(quote);
